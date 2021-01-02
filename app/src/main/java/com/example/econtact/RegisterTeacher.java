5d458011bdc47c77d7c0468b39a39bd3ab30eb69
adkwas <kwasniaczek96@gmail.com>
2021-01-03 00:19:09 +0100

    stage 02.01.2021 - 2

--- app/src/main/java/com/example/econtact/RegisterTeacher.java
+++ app/src/main/java/com/example/econtact/RegisterTeacher.java
@@ -85,7 +85,7 @@ public class RegisterTeacher extends AppCompatActivity {
         fieldUniversity = findViewById(R.id.spinnerField_RegisterTeacher);
 
         setPicture = findViewById(R.id.setPicture_RegisterTeacher);
-        pictureTeacher = findViewById(R.id.imageView1_OngoingTicketStudent);
+        pictureTeacher = findViewById(R.id.imageView1_ConfirmTickets);
 
         storageReference = FirebaseStorage.getInstance().getReference();
 
