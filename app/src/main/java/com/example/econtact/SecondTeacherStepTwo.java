c9899014b067005655e7bdcdefcdc510dfbe3e3c
adkwas <kwasniaczek96@gmail.com>
2021-01-04 18:05:34 +0100

    stage 04.01.2021 - 19

--- app/src/main/java/com/example/econtact/SecondTeacherStepTwo.java
+++ app/src/main/java/com/example/econtact/SecondTeacherStepTwo.java
@@ -4,11 +4,14 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,14 +24,24 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import io.grpc.Context;
 
 public class SecondTeacherStepTwo extends AppCompatActivity {
 
     TextView nameTeacher2, surnameTeacher2, fieldTeacher2, facultyTeacher2, emailTeacher2;
     Button sendTicket, previousStep;
+
+    ImageView imageViewTeacher;
+    StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +56,34 @@ public class SecondTeacherStepTwo extends AppCompatActivity {
         sendTicket = findViewById(R.id.nextStep_secondTeacherStepTwo);
         previousStep = findViewById(R.id.previousStep_secondTeacherStepTwo);
 
+        imageViewTeacher = findViewById(R.id.imageView_SecondTeacherStepTwo);
+
+
         nameTeacher2.setText(getIntent().getStringExtra("nameTeacher2"));
         surnameTeacher2.setText(getIntent().getStringExtra("surnameTeacher2"));
         facultyTeacher2.setText(getIntent().getStringExtra("facultyTeacher2"));
         fieldTeacher2.setText(getIntent().getStringExtra("fieldTeacher2"));
         emailTeacher2.setText(getIntent().getStringExtra("emailTeacher2"));
 
+        storageReference = FirebaseStorage.getInstance().getReference().child( getIntent().getStringExtra("emailTeacher2")+ ".jpg");
+
+        try {
+            final File localFile = File.createTempFile(getIntent().getStringExtra("emailTeacher2"), "jpg");
+            storageReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    imageViewTeacher.setImageBitmap(bitmap);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         sendTicket.setOnClickListener(new View.OnClickListener() {
             @Override
